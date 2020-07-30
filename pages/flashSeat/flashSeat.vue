60fb204fe110b9b0046c86c4ee6f42632fe46889
irvingming11 <irvingming11@sina.com>
2020-07-30 19:48:40 +0800

    上传项目雏形

---
+++ pages/flashSeat/flashSeat.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>座位</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/reserveList/reserveList.vue
@@ -0,0 +1,11 @@
+<template>
+	<view>
+		预约记录
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
